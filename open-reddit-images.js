dddcee533decdf8fbb49b19bcc88ee907b666cb8
Priyav Shah <priyavrocks@gmail.com>
2018-04-30 23:38:21 +0100

    Added readme.md

--- open-reddit-images.js
+++ open-reddit-images.js
@@ -1,7 +1,6 @@
 document.body.style.border = "5px solid red";
 var elements = document.getElementsByClassName("expando-button");
 
-console.log(elements);
 for (var i = 0; i < elements.length; i++) {
   var element = elements[i];
   element.click();
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Simple Reddit image expander
+Simple reddit image expander.
+Does what it says on the tin.
